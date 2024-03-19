1229943d7cec82f966c4c6a7fba2fe74202b21c9
Reyflect <62106437+Reyflect@users.noreply.github.com>
2024-03-19 23:23:46 +0800

    EventSystem

---
+++ Assets/Scripts/Interfaces/IEvent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IEvent : MonoBehaviour
+{
+    // Probability of triggering the action
+    [Range(0f, 1f)]
+    public float triggerProbability = 1f;
+
+    // Abstract method to define the action to be performed when triggered
+    protected abstract void PerformAction();
+
+    // Abstract method to define the conditions for triggering the action
+    protected abstract bool ShouldTrigger(Collider other);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object entering the trigger zone meets the conditions
+        // Here, you can check for specific tags or layers if needed
+        if (ShouldTrigger(other))
+        {
+            // Trigger the action based on probability
+            if (Random.value < triggerProbability)
+            {
+                // Perform the action
+                PerformAction();
+
+               
+            }
+
+            // Destroy the object since the trigger should happen only once
+            Destroy(gameObject);
+        }
+        
+    }
+
+}
+
---
+++ Assets/Scripts/LevelEvents/SampleEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleEvent : IEvent
+{
+    protected override void PerformAction()
+    {
+        Debug.Log("Event triggered! Performing action...");
+    }
+
+    protected override bool ShouldTrigger(Collider other)
+    {
+        return other.CompareTag("Player");
+    }
+}
